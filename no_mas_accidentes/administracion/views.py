1b82691ffb8bd05a7046c943c21690b21889af13
Luis Correa <luis.correa@uber.com>
2022-09-05 23:53:45 -0300

    chore(administracion): agregar listar y crear clientes

--- no_mas_accidentes/administracion/views.py
+++ no_mas_accidentes/administracion/views.py
@@ -1,11 +1,37 @@
-from django.views.generic import TemplateView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, TemplateView
 
 from no_mas_accidentes.administracion.constants import app_name
+from no_mas_accidentes.administracion.forms import CrearClienteForm
 from no_mas_accidentes.administracion.mixins import EsAdministradorMixin
+from no_mas_accidentes.users.models import User
 
 
-class Home(EsAdministradorMixin, TemplateView):
+class HomeView(EsAdministradorMixin, TemplateView):
     template_name = f"{app_name}/home.html"
 
 
-home_view = Home.as_view()
+class ListaClientesView(EsAdministradorMixin, ListView):
+    queryset = User.objects.filter(groups__name="cliente").all()
+    fields = "__all__"
+    success_url = reverse_lazy(f"{app_name}:mantenedor_clientes_lista")
+    ordering = "id"
+    paginate_by = 10
+    template_name = f"{app_name}/lista_clientes.html"
+
+
+class CrearClienteView(EsAdministradorMixin, SuccessMessageMixin, CreateView):
+    template_name = f"{app_name}/crear_cliente.html"
+    form_class = CrearClienteForm
+    success_url = reverse_lazy(f"{app_name}:mantenedor_clientes_lista")
+
+    def get_success_message(self, cleaned_data):
+        return (
+            f"Cliente {self.object.id} - {self.object.email} creado satisfactoriamente"
+        )
+
+
+home_view = HomeView.as_view()
+lista_clientes_view = ListaClientesView.as_view()
+crear_cliente_view = CrearClienteView.as_view()
