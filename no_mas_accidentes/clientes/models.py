95d575c300a9814959a90ca3de056da0b7b284f7
Luis Correa <luis.correa@uber.com>
2022-09-06 22:28:28 -0300

    chore(clientes): agregar telefono y profesional asignado a empresa

--- no_mas_accidentes/clientes/models.py
+++ no_mas_accidentes/clientes/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from no_mas_accidentes.users import validators as users_validators
@@ -13,6 +14,14 @@ class Empresa(models.Model):
     latitud = models.FloatField(blank=True, null=True)
     longitud = models.FloatField(blank=True, null=True)
     esta_activa = models.BooleanField(default=True)
+    telefono = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+        validators=[MinValueValidator(900000000), MaxValueValidator(999999999)],
+    )
+    profesional_asignado = models.ForeignKey(
+        "profesionales.Profesional", blank=True, null=True, on_delete=models.SET_NULL
+    )
 
     def __str__(self):
         return f"{self.rut} - {self.nombre}"
