e3c83e9319f1510630e98f6492afbe27da9d397b
Luis Correa <luis.correa@uber.com>
2022-09-07 21:46:08 -0300

    chore(administracion): agregar mixin PassRequestToFormViewMixin

--- no_mas_accidentes/administracion/mixins.py
+++ no_mas_accidentes/administracion/mixins.py
@@ -4,3 +4,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 class EsAdministradorMixin(LoginRequiredMixin, UserPassesTestMixin):
     def test_func(self):
         return self.request.user.groups.filter(name="administrador").exists()
+
+
+class PassRequestToFormViewMixin:
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs["request"] = self.request
+        return kwargs
