e90c6e9b0c8570730648da0bb45442e49423783c
Luis Correa <luis.correa@uber.com>
2022-09-08 01:42:26 -0300

    chore(clientes): agregar Contrato a Django admin

--- no_mas_accidentes/clientes/admin.py
+++ no_mas_accidentes/clientes/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from no_mas_accidentes.clientes.models import Empresa
+from no_mas_accidentes.clientes.models import Contrato, Empresa
 
 
 @admin.register(Empresa)
 class EmpresaAdmin(admin.ModelAdmin):
     list_display = ["rut", "nombre", "giro", "esta_activa"]
+
+
+@admin.register(Contrato)
+class ContratoAdmin(admin.ModelAdmin):
+    list_display = ["id", "creado_en", "esta_activo"]
