4380b8150b83ad6251042a1a2a4949c78cf046bb
Luis Correa <luis.correa@uber.com>
2022-09-09 17:52:49 -0300

    chore(profesionales): agregar HorarioProfesional, borrar Servicio

--- no_mas_accidentes/profesionales/models.py
+++ no_mas_accidentes/profesionales/models.py
@@ -1,7 +1,6 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-from no_mas_accidentes.clientes.models import Empresa
 from no_mas_accidentes.users.models import User
 
 
@@ -17,35 +16,9 @@ class Profesional(models.Model):
         return f"{self.usuario.rut} - {self.usuario.name}"
 
 
-# class HorarioProfesional(models.Model):
-#     fecha_inicio = models.DateField()
-#     fecha_termino = models.DateField()
-#     desde = models.TimeField()
-#     hasta = models.TimeField()
-
-
-class Servicio(models.Model):
-    TIPOS = [
-        ("ASESORIA EMERGENCIA", "ASESORIA EMERGENCIA"),
-        ("ASESORIA FISCALIZACION ", "ASESORIA FISCALIZACION"),
-        ("CAPACITACION", "CAPACITACION"),
-        ("VISITA", "VISITA"),
-        ("LLAMADA", "LLAMADA"),
-    ]
-    tipo = models.CharField(choices=TIPOS, max_length=256)
+class HorarioProfesional(models.Model):
+    fecha_inicio = models.DateField()
+    fecha_termino = models.DateField()
+    desde = models.TimeField()
+    hasta = models.TimeField()
     profesional = models.ForeignKey(Profesional, null=True, on_delete=models.SET_NULL)
-    empresa = models.ForeignKey(Empresa, null=True, on_delete=models.SET_NULL)
-    creado_en = models.DateTimeField(auto_now_add=True)
-    actualizado_en = models.DateTimeField(auto_now=True)
-
-    agendado_para = models.DateTimeField(null=True, blank=True)
-    realizado_en = models.DateTimeField(null=True, blank=True)
-    num_participantes = models.PositiveIntegerField(
-        null=True,
-        blank=True,
-        validators=[MaxValueValidator(10_000)],
-    )
-    motivo = models.TextField(null=True, blank=True)
-    contenido = models.TextField(null=True, blank=True)
-    materiales = models.TextField(null=True, blank=True)
-    duracion = models.DurationField(null=True, blank=True)
