57d2b19fb60eb0a269083a152772fc33fc3055c3
Luis Correa <luis.correa@uber.com>
2022-09-09 02:42:52 -0300

    chore(administracion): agregar metodo usuario_es_administrador

--- no_mas_accidentes/administracion/mixins.py
+++ no_mas_accidentes/administracion/mixins.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import AbstractUser
+
+
+def usuario_es_administrador(user: AbstractUser) -> bool:
+    return user.groups.filter(name="administrador").exists()
 
 
 class EsAdministradorMixin(LoginRequiredMixin, UserPassesTestMixin):
     def test_func(self):
-        return self.request.user.groups.filter(name="administrador").exists()
+        return usuario_es_administrador(self.request.user)
 
 
 class PassRequestToFormViewMixin:
