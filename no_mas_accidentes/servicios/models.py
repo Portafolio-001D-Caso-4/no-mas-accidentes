d8d6263c759db4bbd86462c572322e4b99c0ba21
Luis Correa <luis.correa@uber.com>
2022-09-09 17:53:36 -0300

    chore(servicios): agregar Servicio

---
+++ no_mas_accidentes/servicios/models.py
@@ -0,0 +1,32 @@
+from django.core.validators import MaxValueValidator
+from django.db import models
+
+from no_mas_accidentes.clientes.models import Empresa
+from no_mas_accidentes.profesionales.models import Profesional
+
+
+class Servicio(models.Model):
+    TIPOS = [
+        ("ASESORIA EMERGENCIA", "ASESORIA EMERGENCIA"),
+        ("ASESORIA FISCALIZACION ", "ASESORIA FISCALIZACION"),
+        ("CAPACITACION", "CAPACITACION"),
+        ("VISITA", "VISITA"),
+        ("LLAMADA", "LLAMADA"),
+    ]
+    tipo = models.CharField(choices=TIPOS, max_length=256)
+    profesional = models.ForeignKey(Profesional, null=True, on_delete=models.SET_NULL)
+    empresa = models.ForeignKey(Empresa, null=True, on_delete=models.SET_NULL)
+    creado_en = models.DateTimeField(auto_now_add=True)
+    actualizado_en = models.DateTimeField(auto_now=True)
+
+    agendado_para = models.DateTimeField(null=True, blank=True)
+    realizado_en = models.DateTimeField(null=True, blank=True)
+    num_participantes = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+        validators=[MaxValueValidator(10_000)],
+    )
+    motivo = models.TextField(null=True, blank=True)
+    contenido = models.TextField(null=True, blank=True)
+    materiales = models.TextField(null=True, blank=True)
+    duracion = models.DurationField(null=True, blank=True)
