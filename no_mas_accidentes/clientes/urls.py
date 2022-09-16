e539755aaeb5a60635cf8cd771ec717ac56ca341
Luis Correa <luis.correa@uber.com>
2022-09-16 04:44:59 -0300

    chore(clientes): crear template de empresa adeudada

--- no_mas_accidentes/clientes/urls.py
+++ no_mas_accidentes/clientes/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
 
-from no_mas_accidentes.clientes.views import home_view
+from no_mas_accidentes.clientes.views import (
+    empresa_adeudada_view,
+    home_view,
+    realizar_pago_view,
+    transaccion_exitosa_view,
+)
 
 app_name = "clientes"
 urlpatterns = [
     path("home/", view=home_view, name="home"),
+    path("realizar-pago/", view=realizar_pago_view, name="realizar_pago"),
+    path("empresa-adeudada/", view=empresa_adeudada_view, name="empresa_adeudada"),
+    path(
+        "transaccion-exitosa/",
+        view=transaccion_exitosa_view,
+        name="transaccion_exitosa",
+    ),
 ]
--- no_mas_accidentes/clientes/views.py
+++ no_mas_accidentes/clientes/views.py
@@ -1,9 +1,14 @@
 import datetime
 
+import arrow
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
+from transbank.webpay.webpay_plus.transaction import Transaction
 
 from no_mas_accidentes.clientes.constants import app_name
-from no_mas_accidentes.clientes.mixins import EsClienteMixin
+from no_mas_accidentes.clientes.mixins import EsClienteMixin, EsClienteYAdeudadoMixin
 from no_mas_accidentes.clientes.models import FacturaMensual
 from no_mas_accidentes.servicios.business_logic.servicios import (
     traer_context_data_de_servicios_por_empresa,
@@ -11,7 +16,7 @@ from no_mas_accidentes.servicios.business_logic.servicios import (
 from no_mas_accidentes.servicios.models import Evento
 
 
-class Home(EsClienteMixin, TemplateView):
+class Home(EsClienteYAdeudadoMixin, TemplateView):
     template_name = f"{app_name}/home.html"
 
     def get_context_data(self, **kwargs):
@@ -49,4 +54,65 @@ class Home(EsClienteMixin, TemplateView):
         return context
 
 
+class EmpresaAdeudadaView(EsClienteMixin, TemplateView):
+    template_name = f"{app_name}/empresa-adeudada.html"
+
+
+class RealizarPagoView(EsClienteMixin, TemplateView):
+    template_name = f"{app_name}/pagos/realizar-pago.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        amount = 100000
+        buy_order = str(arrow.utcnow().int_timestamp)
+        session_id = str(self.request.user.id)
+        return_url = self.request.build_absolute_uri(
+            location=reverse("clientes:transaccion_exitosa")
+        )
+        response = Transaction.create(buy_order, session_id, amount, return_url)
+        context["transbank_url"] = response.url
+        context["transbank_token"] = response.token
+
+        return context
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class TransaccionExitosaView(EsClienteMixin, TemplateView):
+    template_name = f"{app_name}/pagos/transaccion-exitosa.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        #
+        # amount = 100000
+        # buy_order = str(arrow.utcnow().int_timestamp)
+        # session_id = str(self.request.user.id)
+        # return_url = self.request.build_absolute_uri(location=reverse("clientes:transaccion_exitosa"))
+        # response = Transaction.create(buy_order, session_id, amount, return_url)
+        # context['transbank_url'] = response.url
+        # context['transbank_token'] = response.token
+
+        return context
+
+    def post(self, request, *args, **kwargs):
+        token = request.POST.get("token_ws")
+        response = Transaction.commit(token=token)
+        context = {
+            "buy_order": response.buy_order,
+            "session_id": response.session_id,
+            "amount": response.amount,
+            "response": response,
+            "token_ws": token,
+            "status": response.status,
+            "fecha_transaccion": response.transaction_date,
+        }
+        # revisar response.status_code para redirigir a otra template
+        # response.response_code == 0
+        # response.status == "AUTHORIZED"
+        return self.render_to_response(context)
+
+
 home_view = Home.as_view()
+empresa_adeudada_view = EmpresaAdeudadaView.as_view()
+realizar_pago_view = RealizarPagoView.as_view()
+transaccion_exitosa_view = TransaccionExitosaView.as_view()
