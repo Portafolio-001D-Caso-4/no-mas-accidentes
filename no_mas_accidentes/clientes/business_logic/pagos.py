9d640ef9a918538a62a47e830fe5dcedfb0980d8
Luis Correa <luis.correa@uber.com>
2022-09-18 00:58:06 -0300

    chore(clientes): agregar realizar_pago_ultima_factura

---
+++ no_mas_accidentes/clientes/business_logic/pagos.py
@@ -0,0 +1,18 @@
+from no_mas_accidentes.clientes.models import FacturaMensual
+from no_mas_accidentes.clientes.tasks import enviar_alerta_pago_realizado
+from no_mas_accidentes.users.models import User
+
+
+def realizar_pago_ultima_factura(id_cliente: int):
+    usuario = User.objects.get(id=id_cliente)
+    empresa_id = usuario.empresa_id
+    factura_actual = FacturaMensual.objects.filter(
+        contrato__empresa_id=empresa_id
+    ).last()
+    factura_actual.forma_pago = "WEBPAY"
+    factura_actual.pagado_por = usuario
+    factura_actual.es_pagado = True
+    factura_actual.save()
+    enviar_alerta_pago_realizado.delay(
+        usuario_id=usuario.id, empresa_id=empresa_id, factura_id=factura_actual.id
+    )
