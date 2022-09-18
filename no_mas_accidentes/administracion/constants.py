035fbe97c6d2742df5adacb9c8700ecd82f0ebc5
Luis Correa <luis.correa@uber.com>
2022-09-17 21:46:35 -0300

    chore(administracion): agregar INFORMACION_EMPRESA_PREVENCION_CHILE

--- no_mas_accidentes/administracion/constants.py
+++ no_mas_accidentes/administracion/constants.py
@@ -6,3 +6,11 @@ class InformacionEmpresaPrevencionChile:
     rut = "101112222"
     direccion = "José Miguel Carrera 1390, Santiago Centro"
     servicio = "ASESORÍA Y SERVICIOS PREVENCIÓN DE RIESGOS"
+
+
+INFORMACION_EMPRESA_PREVENCION_CHILE = {
+    "nombre": InformacionEmpresaPrevencionChile.nombre,
+    "rut": InformacionEmpresaPrevencionChile.rut,
+    "direccion": InformacionEmpresaPrevencionChile.direccion,
+    "servicio": InformacionEmpresaPrevencionChile.servicio,
+}
