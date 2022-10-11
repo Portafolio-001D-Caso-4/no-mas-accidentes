c46c7a37f8e56c8c5cc291c782f01d11273260c2
Luis Correa <luis.correa@uber.com>
2022-10-11 19:33:13 -0300

    chore(profesionales): calcular accidentabilidad por profesional

---
+++ no_mas_accidentes/profesionales/business_logic/accidentabilidad.py
@@ -0,0 +1,31 @@
+import arrow
+from django.db.models.aggregates import Count
+
+from no_mas_accidentes.servicios.constants import TiposDeServicio
+from no_mas_accidentes.servicios.models import Servicio
+
+
+def calcular_accidentabilidad_por_profesional(profesional_id: int) -> float:
+    mes_actual = arrow.utcnow().datetime.month
+    cantidad_servicios_por_tipo = list(
+        Servicio.objects.filter(
+            profesional__usuario__pk=profesional_id, realizado_en__month=mes_actual
+        )
+        .values("tipo")
+        .order_by("tipo")
+        .annotate(count=Count("tipo"))
+    )
+    cantidad_total_servicios = sum(
+        cantidad_servicios_por_tipo["count"]
+        for cantidad_servicios_por_tipo in cantidad_servicios_por_tipo
+        if cantidad_servicios_por_tipo["tipo"]
+        in (TiposDeServicio.ASESORIA_EMERGENCIA, TiposDeServicio.ASESORIA_FISCALIZACION)
+    )
+    cantidad_de_accidentes = sum(
+        cantidad_servicios_por_tipo["count"]
+        for cantidad_servicios_por_tipo in cantidad_servicios_por_tipo
+        if cantidad_servicios_por_tipo["tipo"] == TiposDeServicio.ASESORIA_EMERGENCIA
+    )
+    if cantidad_total_servicios == 0:
+        return 0
+    return round(cantidad_de_accidentes * 100 / cantidad_total_servicios, 2)
