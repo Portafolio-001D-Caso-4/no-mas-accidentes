e1b1e1ba3f701c43329ffbfc6c8736967f85ee05
Luis Correa <luis.correa@uber.com>
2022-10-30 12:09:35 -0300

    chore(profesionales): generar horarios mas seguidos

--- no_mas_accidentes/profesionales/tasks.py
+++ no_mas_accidentes/profesionales/tasks.py
@@ -1,6 +1,6 @@
-import calendar
-import datetime
 import logging
+
+import arrow
 
 from config import celery_app
 from no_mas_accidentes.profesionales.business_logic import horarios_profesional
@@ -14,19 +14,21 @@ def crear_periodicamente_horarios_profesionales():
     profesionales = Profesional.objects.all()
     horarios = []
     for profesional in profesionales:
-        dias_buscados = (
-            range(calendar.MONDAY, calendar.SATURDAY)
-            if profesional.pk % 2 == 0
-            else range(calendar.WEDNESDAY, calendar.FRIDAY)
-        )
-        desde, hasta = (10, 16) if profesional.pk % 2 == 0 else (16, 20)
+        # dias_buscados = (
+        #     range(calendar.MONDAY, calendar.SATURDAY)
+        #     if profesional.pk % 2 == 0
+        #     else range(calendar.WEDNESDAY, calendar.FRIDAY)
+        # )
+        # desde, hasta = (10, 16) if profesional.pk % 2 == 0 else (16, 20)
         cantidad_de_semanas = 2 if profesional.pk % 2 == 0 else 1
+        dias_buscados = (0, 1, 2, 3, 4, 5, 6)
+        desde, hasta = (1, 23)
         horarios.extend(
             horarios_profesional.generar_horario_profesional(
                 id_profesional=profesional.pk,
                 dias_buscados=(tuple(list(dias_buscados))),
                 cantidad_de_semanas=cantidad_de_semanas,
-                dia_inicio=datetime.date.today(),
+                dia_inicio=arrow.utcnow().shift(days=-1).date(),
                 desde=desde,
                 hasta=hasta,
             )
